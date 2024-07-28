b0dae79705daf87705b243078646b690d9be09bc
Shinichii <alden.medium@gmail.com>
2024-07-28 15:22:16 +0200

    MOAR WINCONF

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,2 +1,4 @@
 winget install --id Microsoft.WindowsTerminal -e
 cp ./settings.json $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+winget install JanDeDobbeleer.OhMyPosh -s winget
+cp ./Microsoft.Powershell_profile.ps1 $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1 
